e94ffaf090b0f1324b28d9c3698ae61c8e28db1d
beckdl <icefire24@hotmail.com>
2025-09-22 13:58:49 -0600

    Connection to the Database

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const env = require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const {MongoClient} = require('mongodb');
+
+async function main() {
+  const uri = process.env.DATABASE_URL;
+  
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
 
 app.use('/', routes);
 
