1956c452616a81a2cd3b3815b0dba64ac96d6e16
beckdl <icefire24@hotmail.com>
2025-09-27 22:33:47 -0600

    Updated beginning message

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,5 @@
 const getName = (req, res) => {
-  res.send('Hello,');
-  res.send('Welcome to the Contacts API');
-  res.send('To get all contacts, navigate to /contacts');
+  res.send('Hello, welcome to the Contacts API. To get all contacts, navigate to /contacts.');
 }
 
 module.exports = {getName};
