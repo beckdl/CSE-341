f2500abac9328c09a4860ab75011325f4d5f2eb7
beckdl <icefire24@hotmail.com>
2025-10-10 12:25:41 -0600

    Fix for UI CSS issues

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger.json');
 
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css';
+
 router.use('/api-docs', swaggerUi.serve)
-router.get('/api-docs', swaggerUi.setup(swaggerFile));
+router.get('/api-docs', swaggerUi.setup(swaggerFile, null, null, CSS_URL));
 
 module.exports = router;
