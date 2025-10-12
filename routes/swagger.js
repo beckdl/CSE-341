99bbfbd68566215038bcb2bd9115d2a53902329e
beckdl <icefire24@hotmail.com>
2025-10-11 20:17:50 -0600

    Revert "Fix for UI CSS issues"
    This reverts commit f2500abac9328c09a4860ab75011325f4d5f2eb7.

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger.json');
 
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css';
-
 router.use('/api-docs', swaggerUi.serve)
-router.get('/api-docs', swaggerUi.setup(swaggerFile, null, null, CSS_URL));
+router.get('/api-docs', swaggerUi.setup(swaggerFile));
 
 module.exports = router;
